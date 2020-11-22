14c5ada195b17e8327a34240e15468278142da41
xtrdnrmnd <polinkakub@mail.ru>
2020-11-22 15:56:36 +0100

    Added the delete for the collection mongo

--- src/main/java/MongoDB.java
+++ src/main/java/MongoDB.java
@@ -17,30 +17,35 @@ public class MongoDB {
             Scanner in = new Scanner(System.in);
             num = in.nextInt();
             switch (num) {
-                case 1:
+                case 1: // Add a document to the collection
                     Objects object = new Objects();
                     System.out.println("Введите айди студента: ");
                     int id = in.nextInt();
-                    object.setStd_id(23456);
+                    object.setStd_id(id);
                     System.out.println("Введите имя студента: ");
                     String fio = in.next();
-                    object.setStd_fullname("sdsd");
+                    object.setStd_fullname(fio);
                     System.out.println("Введите логин студента: ");
                     String login = in.next();
-                    object.setStd_login("sdsdsd");
+                    object.setStd_login(login);
                     System.out.println("Введите пароль студента: ");
                     String pass = in.next();
-                    object.setStd_pswd("sdsdds");
+                    object.setStd_pswd(pass);
                     students.insert(convert(object));
                     DBObject query = new BasicDBObject("_id", object.getStd_id());
                     DBCursor cursor = students.find(query);
-                    System.out.println(cursor.one());
-                case 2:
-                case 3:
-                    //DBObject query = new BasicDBObject("ID", 123);
-                    //DBCursor cursor = students.find(query);
-                    //System.out.println(cursor.one());
-                case 4:
+                    System.out.println("Пользователь "+cursor.one()+" добавлен в коллекцию!");
+                    break;
+                case 2: // Delete a document from the collection
+                case 3: // View all the documents in the collection
+                    DBObject query2 = new BasicDBObject();
+                    DBCursor cursor2 = students.find(query2);
+                    System.out.println("Все записи в коллекции:");
+                    while (cursor2.hasNext()) {
+                        DBObject next = cursor2.next();
+                        System.out.println(next);
+                    }
+                case 4: // Modify the document in the collection
             }
         } while (num!=5);
     }
