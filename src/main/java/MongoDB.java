5baf3194288bb76d0401d0017d276cd7cd904f2a
xtrdnrmnd <polinkakub@mail.ru>
2020-11-22 16:39:57 +0100

    add,modify,delete and show the documents

--- src/main/java/MongoDB.java
+++ src/main/java/MongoDB.java
@@ -1,3 +1,5 @@
+//xtrdnrmnd 2020
+
 import com.mongodb.*;
 import java.net.UnknownHostException;
 import java.util.Scanner;
@@ -37,15 +39,49 @@ public class MongoDB {
                     System.out.println("Пользователь "+cursor.one()+" добавлен в коллекцию!");
                     break;
                 case 2: // Delete a document from the collection
-                case 3: // View all the documents in the collection
-                    DBObject query2 = new BasicDBObject();
+                    System.out.println("Укажите id пользователя, данные которого вы хотите удалить:");
+                    int id_delete = in.nextInt();
+                    DBObject query2 = new BasicDBObject("_id", id_delete);
                     DBCursor cursor2 = students.find(query2);
+                    if (cursor2.one() == null)
+                        System.out.println("Пользователя с таким айди не существует");
+                    else {
+                        students.findAndRemove(query2);
+                        System.out.println("Пользователь удален!");
+                        break;
+                    }
+                case 3: // View all the documents in the collection
+                    DBObject query3 = new BasicDBObject();
+                    DBCursor cursor3 = students.find(query3);
                     System.out.println("Все записи в коллекции:");
-                    while (cursor2.hasNext()) {
-                        DBObject next = cursor2.next();
+                    while (cursor3.hasNext()) {
+                        DBObject next = cursor3.next();
                         System.out.println(next);
                     }
+                    break;
                 case 4: // Modify the document in the collection
+                    System.out.println("Укажите id пользователя, данные которого вы хотите изменить:");
+                    int id_change = in.nextInt();
+                    DBObject query4 = new BasicDBObject("_id", id_change);
+                    DBCursor cursor4 = students.find(query4);
+                    if (cursor4.one() == null)
+                        System.out.println("Пользователя с таким айди не существует");
+                    else {
+                        Objects modifier = new Objects();
+                        modifier.setStd_id(id_change);
+                        System.out.println("Введите новую фамилию:");
+                        String new_name = in.next();
+                        modifier.setStd_fullname(new_name);
+                        System.out.println("Введите новый логин: ");
+                        String new_login = in.next();
+                        modifier.setStd_login(new_login);
+                        System.out.println("Введите новый пароль: ");
+                        String new_pswd = in.next();
+                        modifier.setStd_pswd(new_pswd);
+                        students.findAndModify(query4, convert(modifier));
+                        DBCursor cursor5 = students.find(query4);
+                        System.out.println(cursor5.one());
+                    }
             }
         } while (num!=5);
     }
